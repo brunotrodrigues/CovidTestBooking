acffe5be4c656752de6e638fa6096a216f5eeaac
mrbonur7 <brunotrodrigues16@outlook.com>
2021-06-12 07:16:24 +0100

    call me started

---
+++ projeto/js/controllers/messageController.js
@@ -0,0 +1,8 @@
+import MessageModel from '../models/messageModel.js'
+
+export default class MessageController{
+    constructor(){
+        this.message = localStorage.message ? JSON.parse(localStorage.message) : [];
+    }
+    
+}
---
+++ projeto/js/views/messageView.js
@@ -0,0 +1,12 @@
+import MessageController from '../controllers/messageController.js'
+
+export default class MessageView {
+    constructor() {
+        this.messageController = new MessageController();
+
+        this.frmLogin = document.querySelector('#name_contact');
+        this.loginUsername = document.querySelector('#email_contact');
+        this.loginPassword = document.querySelector('#message_contact');
+    
+    }
+}
